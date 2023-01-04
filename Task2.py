fd7d1caecff66908ad6a8476997ad63e4976071d
Saraa7med <114696450+Saraa7med@users.noreply.github.com>
2023-01-04 23:37:19 +0200

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1- API
+# 
+# ###### An API is a set of  that enables data transmission between one software product and another. It also contains the terms of this data exchange. The application programming interface must be clearly distinguished from a user interface. The user interface accepts data from users, forwards it to the application for processing, and returns the results to the user. The API does not interact with the user, but processes the data received from one program module and transmits the results back to the other module.   
+# 
+# 
+# ## Examples of APIs
+#  -Google Maps
+#  
+#  -Facebook
+#  
+#  -Google Translate
+#  
+#  -YouTube
+
+# # 2-PLATFORM
+# 
+
+# # 3-FRAMEWORK
+
+# # ......................................................................................
+
+# # 4-Open source programming languages...
+# 
+# - JavaScript
+# - Python
+# - Java
+# - C ++
+# - R
+# - PHP 
+# - Kotlin 
+# - Perl
+# - Ruby
+# 
+
+# # 5-closed source programming languages...
+# 
+# - SQL
+# - C #
+# - MySQL
